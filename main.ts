47a36e0fd466967affaf12017bfd772b13122f14
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-01-27 11:43:44 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let strip = neopixel.create(DigitalPin.P0, 20, NeoPixelMode.RGB)
 basic.forever(function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.White))
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
     basic.pause(200)
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.pause(200)
 })
